8ab8ec812bd09544b129c94326247b2f0f15c22f
Sujan-2006 <sujan032006@gmail.com>
2025-02-16 17:02:19 +0530

    functionbutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css' 
 
-function App() {
-  alert("Welcome, KONNICHIWA!");
-const name = "SUJAN";
-console.log("my age is 18");
-  return(
-<h1>{name} P</h1>
+function App() { 
+  let name = "SUJAN"
+  
+  
+  const mydetails={  
+    name: "SUJAN",
+    age: 18,
+    dept: "M.Tech Computer Science And Engineering",
+  }
+  
+  alert(`${mydetails.name}`)
+  console.log(`Age is ${mydetails.age}`)
+const sujan=()=>{
+  console.log("I am from M.Tech CSE dept");
+}
+
+  return ( 
+    
+    <>
+      <h1 style={{textAlign:"center"}}>{mydetails.dept}</h1>  {} 
+      
+      {
+       }
+      {}
+      {}
+      <p className='container'>Name: {mydetails.name}<br/>  {}
+      
+        Age: {mydetails.age}<br/>
+        Department: {mydetails.dept}<br/>
+      </p>
+    
+     <button onClick={sujan}>hi, I am here</button>
+     <button onClick={()=>console.log("hiiiiiiii")}>KONNICHIWA</button>
+    </>
   )
 }
 
 export default App
+
